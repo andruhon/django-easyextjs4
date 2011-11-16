aeae783dd0130294d82a9266e450624820263b74
TofPlay <chbperso@gmail.com>
2011-11-16 12:03:16 -0500

    Update demo/website/app/app.js

--- demo/website/app/app.js
+++ demo/website/app/app.js
@@ -17,35 +17,35 @@ Ext.application({
     name: 'EasyExtJS4 - Demo',
     launch: function() {
     	
-			// Create form instance
-			Ext.create('DemoEasyExtJS4.view.Compute');
+		// Create form instance
+		Ext.create('DemoEasyExtJS4.view.Compute');
 
-			// Event			
-			lOnEvent1Cpt = 1;
+		// Event			
+		lOnEvent1Cpt = 1;
 
-			Ext.direct.Manager.on('DemoEasyExtJS4.Compute.Event', function(event){
-				var lMsg = 'Event: [' + lOnEvent1Cpt + '] ' + event.data,
-				    lEventMsg = Ext.getCmp('idEvent'); 
+		Ext.direct.Manager.on('DemoEasyExtJS4.Compute.Event', function(event){
+			var lMsg = 'Event: [' + lOnEvent1Cpt + '] ' + event.data,
+			    lEventMsg = Ext.getCmp('idEvent'); 
             	Ext.log(lMsg);
-				lEventMsg.setText(lMsg);
-        		lOnEvent1Cpt++;
-			});
+			lEventMsg.setText(lMsg);
+			lOnEvent1Cpt++;
+		});
 
-    		// Display a message box if an exception on the server occur
-			Ext.direct.Manager.on('exception', function(e) {
-						if (e == null || e.code == 'xhr') return;
-						if (e.data.Type == 'event'){
-							lPoll = Ext.direct.Manager.getProvider(e.data.Id);
-							lPoll.disconnect();
-						}
-						Ext.log({level: 'error', msg:'Exception: ' + e.message});
-						Ext.Msg.show({
-						           title:'Exception',
-						           msg: e.message,
-						           buttons: Ext.Msg.OK,
-						           icon: Ext.Msg.ERROR
-						    });
-						});
+		// Display a message box if an exception on the server occur
+		Ext.direct.Manager.on('exception', function(e) {
+					if (e == null || e.code == 'xhr') return;
+					if (e.data.Type == 'event'){
+						lPoll = Ext.direct.Manager.getProvider(e.data.Id);
+						lPoll.disconnect();
+					}
+					Ext.log({level: 'error', msg:'Exception: ' + e.message});
+					Ext.Msg.show({
+					           title:'Exception',
+					           msg: e.message,
+					           buttons: Ext.Msg.OK,
+					           icon: Ext.Msg.ERROR
+					    });
+				});
 
 	    }
 });
