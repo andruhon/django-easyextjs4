5810e0b365e724954a70beee35e46250c1ddfeb7
TofPlay <chbperso@gmail.com>
2011-11-16 12:04:41 -0500

    Update demo/website/app/app.js

--- demo/website/app/app.js
+++ demo/website/app/app.js
@@ -26,26 +26,26 @@ Ext.application({
 		Ext.direct.Manager.on('DemoEasyExtJS4.Compute.Event', function(event){
 			var lMsg = 'Event: [' + lOnEvent1Cpt + '] ' + event.data,
 			    lEventMsg = Ext.getCmp('idEvent'); 
-            	Ext.log(lMsg);
+	            	Ext.log(lMsg);
 			lEventMsg.setText(lMsg);
 			lOnEvent1Cpt++;
 		});
 
 		// Display a message box if an exception on the server occur
 		Ext.direct.Manager.on('exception', function(e) {
-					if (e == null || e.code == 'xhr') return;
-					if (e.data.Type == 'event'){
-						lPoll = Ext.direct.Manager.getProvider(e.data.Id);
-						lPoll.disconnect();
-					}
-					Ext.log({level: 'error', msg:'Exception: ' + e.message});
-					Ext.Msg.show({
-					           title:'Exception',
-					           msg: e.message,
-					           buttons: Ext.Msg.OK,
-					           icon: Ext.Msg.ERROR
-					    });
-				});
+			if (e == null || e.code == 'xhr') return;
+			if (e.data.Type == 'event'){
+				lPoll = Ext.direct.Manager.getProvider(e.data.Id);
+				lPoll.disconnect();
+			}
+			Ext.log({level: 'error', msg:'Exception: ' + e.message});
+			Ext.Msg.show({
+			           title:'Exception',
+			           msg: e.message,
+			           buttons: Ext.Msg.OK,
+			           icon: Ext.Msg.ERROR
+			});
+		});
 
-	    }
+	}
 });
